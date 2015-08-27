6380da9f3b0a0a7bbf614aad4529f6143e3c3e10
alucard24 <dmbaoh2@gmail.com>
2015-08-28 01:39:48 +0200

    cpufreq: Introduce Nightmare GOV
    Included :
    https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commits/042e11014240075029990eaaaa9dd44e777e3446/drivers/cpufreq/cpufreq_nightmare.c
    
    * Lowered responsiveness_max
    * Removed MPDecision HACK
    * Changed INIT_DEFERRABLE_WORK to previous name INIT_DELAYED_WORK_DEFERRABLE
    * Added CPUFREQ_RELATION_H to GOV limits
    
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -388,6 +388,9 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
 extern struct cpufreq_governor cpufreq_gov_optimax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_optimax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_nightmare)
 #endif
 
 
