185a609548c75c97a6b2f0c9fbbb63c2b9062b05
nobe1976 <charlesmc76@gmail.com>
2015-10-10 13:48:00 -0400

    Fix cpufreq_ondemandplus.h

--- include/trace/events/cpufreq_ondemandplus.h
+++ include/trace/events/cpufreq_ondemandplus.h
@@ -74,7 +74,3 @@ DEFINE_EVENT(loadeval, cpufreq_ondemandplus_already,
 
 /* This part must be outside protection */
 #include <trace/define_trace.h>
-<<<<<<< HEAD
-=======
-
->>>>>>> eaf3e26... cpufreq: Introduce onDemandPlus GOV
